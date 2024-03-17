feca2599f3163edc8cccb9f0eb621d8ba4cd98e7
sharifok1 <sharifok1@gmail.com>
2024-03-17 23:18:35 +0600

    first commit

---
+++ src/WalletConnection/WalletConnection.jsx
@@ -0,0 +1,12 @@
+import { TonConnectButton } from '@tonconnect/ui-react';
+import React from 'react';
+
+const WalletConnection = () => {
+    return (
+        <div>
+             <TonConnectButton className="my-button-class"/>
+        </div>
+    );
+};
+
+export default WalletConnection;
