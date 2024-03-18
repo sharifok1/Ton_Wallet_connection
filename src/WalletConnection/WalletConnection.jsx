a6ce86891510e89f2921652885ae3a315880e045
sharifok1 <sharifok1@gmail.com>
2024-03-18 16:06:39 +0600

    update manifest

--- src/WalletConnection/WalletConnection.jsx
+++ src/WalletConnection/WalletConnection.jsx
@@ -1,10 +1,25 @@
-import { TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
 import React from 'react';
 
+
+
 const WalletConnection = () => {
+
+    // const [, setConnectRequestParameters] = useTonConnectUI();
+    const [, setConnectRequestParameters] = useTonConnectUI();
+
+    const customizeReturnStrategy = () => {
+        setConnectRequestParameters({
+            actionsConfiguration: {
+                twaReturnUrl: 'https://t.me/wallet_connection' // Customize the return URL
+            }
+        });
+    };
+
+
     return (
         <div>
-             <TonConnectButton className="my-button-class"/>
+            <TonConnectButton className="my-button-class" onClick={customizeReturnStrategy}/>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <TonConnectUIProvider manifestUrl="/manifest.json">
+      <TonConnectUIProvider manifestUrl="https://demoton.netlify.app/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
     
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// tonconnect-manifest.json
